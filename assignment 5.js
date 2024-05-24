4ce863ffd75bf8c1b98851c838dbc851081fb46d
Aneeqa9007 <138521630+Aneeqa9007@users.noreply.github.com>
2024-05-24 16:13:47 +0530

    Add files via upload

---
+++ assignment 5.js
@@ -0,0 +1,30 @@
+// print table of 10
+
+let a = 1
+
+let b = 10
+while (a <= 10) {
+    console.log(b + " X " + a + " =", b * a)
+    a = a + 1
+}
+
+// // prime number
+// let check = 72
+
+// let i = 2
+// let flag = false
+
+// while(i<check){
+//     if(check%i==0){
+//         flag = true
+//     }
+//     i++ 
+// }
+// if(flag == false){
+//     console.log(check,'Is a prime')
+// }else{
+//     console.log(check,'Is not a prime')
+// }
+
+
+
